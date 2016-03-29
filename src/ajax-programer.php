b050b4828e97a57f790ed051f29ad033baffd43d
Radiksman <radion91@gmail.com>
2016-03-29 15:17:27 +0300

    no message

--- src/ajax-programer.php
+++ src/ajax-programer.php
@@ -5,13 +5,17 @@
     if(isset($_POST['info'])) {
         $subject = $_POST['info'];
     }
-    if(isset($_POST['contact_name'])) {
+    if(isset($_POST['contact_name '])) {
         $name = substr(htmlspecialchars(trim($_POST['contact_name'])), 0, 100);
         $mess .= '<b>Имя:</b>' . $name . '<br>';
     }
     if(isset($_POST['contact_tel'])) {
         $tel = substr(htmlspecialchars(trim($_POST['contact_tel'])), 0, 100);
-        $mess .= '<b>Телефон:</b>' . $tel . '<br>';
+        $mess .= '<b>Телефон:</b> ' . $tel . '<br>';
+    }
+    if(isset($_POST['contact_area'])) {
+        $area = substr(htmlspecialchars(trim($_POST['contact_area'])), 0, 100);
+        $mess .= '<b>Сообщение </b> ' . $tel . '<br>';
     }
     if(isset($_POST['contact_email'])) {
         $mail = substr(htmlspecialchars(trim($_POST['contact_email'])), 0, 100);
@@ -22,7 +26,7 @@
     require 'class.phpmailer.php';
 
     $mail = new PHPMailer();
-    $mail->AddAddress('mail@mail.ru','');   // кому - адрес, Имя
+    $mail->AddAddress('sales@redcaviar.ru','');   // кому - адрес, Имя
     $mail->IsHTML(true);                        // выставляем формат письма HTML
     $mail->Subject = $subject; // тема письма
     $mail->CharSet = "UTF-8";                   // кодировка
